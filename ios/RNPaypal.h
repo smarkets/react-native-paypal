b38aa1db616a48d718d18fb00a54b7c13b0d215a
Valerio Ponte <valerio.ponte@gmail.com>
2018-10-10 10:57:15 +0100

    Implement setup method on iOS.

--- ios/RNPaypal.h
+++ ios/RNPaypal.h
@@ -7,7 +7,15 @@
 
 #import "BraintreePaypal.h"
 
-@interface RNPaypal : NSObject <RCTBridgeModule>
+@interface RNPaypal : UIViewController <RCTBridgeModule, BTDropInViewControllerDelegate, BTViewControllerPresentingDelegate>
+
+@property (nonatomic, strong) BTAPIClient *braintreeClient;
+@property (nonatomic, strong) UIViewController *reactRoot;
+
+@property (nonatomic, strong) RCTResponseSenderBlock callback;
+
++ (instancetype)sharedInstance;
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation;
 
 @end
   
--- ios/RNPaypal.m
+++ ios/RNPaypal.m
@@ -1,13 +1,35 @@
 
 #import "RNPaypal.h"
 
-@implementation RNPaypal
-
-- (dispatch_queue_t)methodQueue
-{
-    return dispatch_get_main_queue();
+@implementation RNPaypal {
+    bool runCallback;
 }
+
+static NSString *URLScheme;
+
++ (instancetype)sharedInstance {
+    static RCTBraintree *_sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedInstance = [[RCTBraintree alloc] init];
+    });
+    return _sharedInstance;
+}
+
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(setup:(NSString *)clientToken urlscheme:(NSString*)urlscheme resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+{
+    URLScheme = urlscheme;
+    [BTAppSwitch setReturnURLScheme:urlscheme];
+    self.braintreeClient = [[BTAPIClient alloc] initWithAuthorization:clientToken];
+    if (self.braintreeClient == nil) {
+        reject();
+    }
+    else {
+        resolve();
+    }
+}
 
 @end
   
